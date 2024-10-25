7f6ca76e0573b27f17c4d36f74de21f2a1a4b609
ireemcobann.github.io <irem14114@gmail.com>
2024-10-25 19:50:57 +0300

    Add files via upload

---
+++ assets/js/pricing.js
@@ -0,0 +1,33 @@
+pageViewRange.addEventListener('input', handlePageViewChange);
+billingType.addEventListener('click', changeBillingType);
+
+let billingTypeValue = false;
+
+
+function handlePageViewChange() {
+	if(billingTypeValue) {
+		pricing.innerText = Number(pageViewRange.value) * 12
+		pageViews.innerText = Number(pageViewRange.value * 6.25) *12
+	}else {
+	pricing.innerText = pageViewRange.value
+	pageViews.innerText = pageViewRange.value * 6.25;
+}
+}
+
+
+function changeBillingType() {
+billingTypeValue = !billingTypeValue;
+changePricing ();
+}
+
+function changePricing() {
+	if(billingTypeValue) {
+		pricing.innerText = (Number(pageViewRange.value) * 0.75) * 12;
+        period.innerText ="year";
+		pageViews.innerText = Number(pageViewRange.value * 6.25) * 12;
+	}else {
+		pricing.innerText = pageViewRange.value
+		period.innerText ="month";
+		pageViews.innerText= pageViewRange * 6.25;
+	}
+}
